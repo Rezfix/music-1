5333ffc84d62b67ce6881534f2a19885caee7b3c
Rezfix <44650563+Rezfix@users.noreply.github.com>
2018-11-08 22:54:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ const fs = require('fs');
 const gif = require("gif-search");
 const client = new Discord.Client({disableEveryone: true});
  
-const prefix = "1";
+const prefix = "2";
 /////////////////////////
 ////////////////////////
  
