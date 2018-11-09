c74de838bc63589da036659f21b0ff0e973a7a08
Rezfix <44650563+Rezfix@users.noreply.github.com>
2018-11-09 23:46:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -356,5 +356,20 @@ if (message.content.startsWith(adminprefix + 'setT')) {
    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)//حقوق دايموند كودز
 }
 });
+
+client.on('message', msg => {
+
+    if (msg.content == '1join') {
+        if (msg.member.voiceChannel) {
+
+     if (msg.member.voiceChannel.joinable) {
+         msg.member.voiceChannel.join().then(msg.react('✅'));
+     }
+    }
+}
+})
+client.on('ready', () => { //code bot not leave room voice //Bot Is Online
+    client.channels.get("510531317692104705").join(); //by : iBeAnthonyD
+    });
  
 client.login(process.env.BOT_TOKEN);
